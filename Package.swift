34fc746602597e00be8833d012156d36a8caae2b
runner <runner@Mac-1655975037362.local>
2022-06-23 09:27:39 +0000

    Created automatic release of prose-core-client 0.1.3.

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "_ProseCoreClientFFI",
-      url: "https://github.com/prose-im/prose-wrapper-swift/releases/download/0.1.2/_ProseCoreClientFFI.xcframework.zip",
-      checksum: "093ae206ae3718960f49be6be0502d6d32a1a55d4aba6a7f952d606efe856955"
+      url: "https://github.com/prose-im/prose-wrapper-swift/releases/download/0.1.3/_ProseCoreClientFFI.xcframework.zip",
+      checksum: "35e5c769d851b44f9edf28f77377e17ffde5dde240775c5d92f19842e95dabd7"
     ),
   ]
 )
