6ae66d2eca3c968dae6f111b2f90c4b1c098d335
Anka <runner@sjc20-cw713-3e89eb35-7c98-4457-a67e-ba20946ecd95-325837B2D719.local>
2025-10-28 01:24:43 +0000

    Update Package.swift and Sources for v0.14.0

--- Package.swift
+++ Package.swift
@@ -1,19 +1,32 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Swift Package: ProseSDK
 import PackageDescription
 
 let package = Package(
-  name: "ProseCoreFFI",
-  platforms: [.macOS(.v11), .iOS(.v13)],
-  products: [
-    .library(name: "ProseCoreFFI", targets: ["ProseCoreFFI"]),
-  ],
-  targets: [
-    .target(name: "ProseCoreFFI", dependencies: ["ProseCoreFFIFFI"]),
-    .binaryTarget(
-      name: "ProseCoreFFIFFI",
-      url: "https://github.com/prose-im/prose-wrapper-swift/releases/download/0.13.0/ProseCoreFFI.xcframework.zip",
-      checksum: "cef697da3a3c8809b9eb1279d9dbd1435513eadfcb53c0113f590d86b78b834e"
-    ),
-  ]
+    name: "ProseSDK",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "ProseSDK",
+            targets: ["ProseSDK"]
+        )
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(
+            name: "ProseCore",
+            url: "https://github.com/prose-im/prose-wrapper-swift/releases/download/v0.14.0/ProseSDK-0.14.0.zip",
+            checksum: "dccf6b10bb579d43666f96474e8a795d636132c3d3ca14052c9ca60fd9456827"
+        ),
+        .target(
+            name: "ProseSDK",
+            dependencies: [
+                .target(name: "ProseCore")
+            ]
+        ),
+    ]
 )
-
